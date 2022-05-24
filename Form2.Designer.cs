e836aadb1708427ecf7dd5c288f5de0a0c6efbdf
Dziurawy410 <dziurawy410@gmail.com>
2022-05-24 21:57:28 +0200

    Dodanie logiki planszy z form1 do form2

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(246, 231);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(393, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LimeGreen;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(393, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(323, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Trafienia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(323, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Wynik";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -15,6 +15,60 @@ namespace SAPER___ZALICZENIE
         public Form2()
         {
             InitializeComponent();
+            GeneratorPlanszy();
+        }
+        public void GeneratorPlanszy()
+        {
+            int sec = 0;
+            int min = 0;
+            Random rdn = new Random();
+            int x = rdn.Next(1, 12);
+            int y = rdn.Next(13, 18);
+            int z = rdn.Next(19, 25);
+
+
+            for (int i = 1; i <= 25; i++)
+            {
+                Button btnTemp = new Button();
+                btnTemp.Cursor = System.Windows.Forms.Cursors.Arrow;
+                btnTemp.Name = "btnTemp" + i.ToString();
+                btnTemp.Size = new System.Drawing.Size(40, 40);
+                btnTemp.UseVisualStyleBackColor = true;
+                flowLayoutPanel1.Controls.Add(btnTemp);
+                btnTemp.Click += BtnTemp_Click;
+                if (x == i || y == i || z == i)
+                    btnTemp.Tag = true;
+                else
+                    btnTemp.Tag = false;
+
+            }
+        }
+        public void BtnTemp_Click(object sender, EventArgs e)
+        {
+            Button btnTemp = (Button)sender;
+            bool tag = (bool)btnTemp.Tag;
+
+            if (tag)
+            {
+                btnTemp.BackColor = Color.Red;
+                int score = int.Parse(label4.Text);
+                score++;
+                label4.Text = score.ToString();
+
+                if (score == 1)
+                {
+                    MessageBox.Show("Przegrałeś!", "Wynik:", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+
+            }
+            else
+            {
+                btnTemp.BackColor = Color.Green;
+                int score = int.Parse(label3.Text);
+                score++;
+                label3.Text = score.ToString();
+
+            }
         }
     }
 }
